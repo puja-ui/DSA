9e434307f1f925de1d1b233f8f96ef78de8ea715
Puja Karmakar <55836766+puja-ui@users.noreply.github.com>
2022-03-08 23:05:52 +0530

    Recursion and Backtracking

---
+++ DiamondPattern.cpp
@@ -0,0 +1,36 @@
+//diamond pattern using recursion and backtracking
+#include <iostream>
+using namespace std;
+
+void ds(int i, int n){
+    if(n<=0) return; //base case
+        
+    int g = (n-1)*2; //space limit
+    while(g--) cout<<" ";
+    
+    int c=i;//star limit
+    while(c--) cout<<"* ";
+    
+    cout<<endl;
+    
+    ds(i+2, n-1);//recursion
+    
+    if(n==1) return; //skipping the double call of longest line
+    //backtracking
+    c=i;
+    g = (n-1)*2;
+    while(g--){
+        cout<<" ";
+    }
+    while(c--){
+        cout<<"* ";
+    }
+    cout<<endl;
+}
+
+int main() {
+	// your code goes here
+	int n=4; //no. of line till longest line
+	ds(1, n);
+	return 0;
+}
